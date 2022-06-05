c1ad3e54f7a33112de1ea6246d90a754647258f5
Kadir Mergen <Kadir Mergen@KADIR-MERGEN>
2022-06-05 16:18:38 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            // şeklinde her kurs geldiğinde buraya eklemek yerine
+            // biz array leri yani dizileri kullanırız.
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı",
+                "Programlamaya başlangıç için temel kurs", "Java","Pyhton","C++" };
+            
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            //bir başka döngü yöntemi
+
+            Console.WriteLine("for bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
